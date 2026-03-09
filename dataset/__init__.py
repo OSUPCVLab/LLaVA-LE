1f8e251a069b93d675a59069eb2c2c2549095a21
Gokce Inal <gokceinal@cardinal-rw01.ten.osc.edu>
2026-03-08 23:34:55 -0400

    removed dataset caption generation script, wrote a general readme doc

--- dataset/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Lunar dataset package initialization.
-
-This package provides dataset classes for loading and processing lunar surface imagery data.
-"""
-
-__version__ = "0.1.0"
-__author__ = "LLaVA-LE Project"
-__description__ = "Lunar surface imagery dataset and caption generation tools"
